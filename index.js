afe673c13981f0191caaf1f695ab0b8e7e6e606e
Sultonov557 <sultonovzerifboy@gmail.com>
2023-09-26 22:31:08 +0500

    .

--- index.js
+++ index.js
@@ -7,33 +7,61 @@ const server = new Server({ port: 81 });
 app.listen(80);
 app.use(express.static(__dirname + "/public"));
 
-const players = [];
+const players = {};
 
 server.on("connection", (socket) => {
-    console.log("connected");
-    let packet = { name: "start", params: { id: players.length } };
-    socket.send(JSON.stringify(packet));
-    players.push(new Player(players.length, new Position(50, 50)));
-    for (i in players) {
-        let packet = { name: "position", params: { position: players[i].position, id: players[i].id } };
-        socket.send(JSON.stringify(packet));
-    }
-    socket.on("message", (data) => {
-        data = JSON.parse(data);
-        console.log(data);
-    });
+	let id;
+	randomID();
+	function randomID() {
+		id = Math.floor(Math.random() * 100);
+		if (players[id]) {
+			randomID();
+		}
+	}
+	let startPacket = { name: "start", params: { id } };
+	console.log("connected " + id);
+	socket.send(JSON.stringify(startPacket));
+	players[id] = new Player(id, new Position(50, 50));
+	for (i in players) {
+		let packet = {
+			name: "position",
+			params: { position: players[i].position, id: players[i].id },
+		};
+		socket.send(JSON.stringify(packet));
+	}
+	socket.on("message", (data) => {
+		data = JSON.parse(data);
+		if (data.name == "position") {
+			let packet = {
+				name: "position",
+				params: { position: data.params.position, id: data.params.id },
+			};
+			broadcast(packet);
+		}
+	});
+	socket.on("close", () => {
+		delete players[id];
+		broadcast({ name: "remove", params: { id } });
+		console.log("disconnected " + id);
+	});
+
+	function broadcast(message) {
+		server.clients.forEach((client) => {
+			client.send(JSON.stringify(message));
+		});
+	}
 });
 
 class Player {
-    constructor(id, position) {
-        this.id = id;
-        this.position = position;
-    }
+	constructor(id, position) {
+		this.id = id;
+		this.position = position;
+	}
 }
 
 class Position {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
 }
--- public/script.js
+++ public/script.js
@@ -4,28 +4,58 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 let id;
-const players = [];
+const players = {};
 
 setInterval(frame, 10);
 
 client.addEventListener("message", (data) => {
-    data = JSON.parse(data.data);
-    if (data.name == "start") {
-        id = data.params.id;
-    }
-    if (data.name == "position") {
-        if (!players[data.params.id]) players[data.params.id] = {};
-        players[data.params.id].position = data.params.position;
-    }
+	data = JSON.parse(data.data);
+	if (data.name == "start") {
+		id = data.params.id;
+	}
+	if (data.name == "position") {
+		if (!players[data.params.id]) players[data.params.id] = {};
+		players[data.params.id].position = data.params.position;
+	}
+	if (data.name == "remove") {
+		delete players[data.params.id];
+		console.log("removed " + data.params.id);
+	}
 });
 
+document.addEventListener("keypress", (ev) => {
+	if (ev.key == "w") {
+		players[id].position.y -= 1;
+	}
+	if (ev.key == "s") {
+		players[id].position.y += 1;
+	}
+	if (ev.key == "a") {
+		players[id].position.x -= 1;
+	}
+	if (ev.key == "d") {
+		players[id].position.x += 1;
+	}
+});
+
+setInterval(() => {
+	client.send(
+		JSON.stringify({ name: "position", params: { id, position: players[id].position } })
+	);
+}, 10);
+
 function frame() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "#151515";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "red";
-    for (i in players) {
-        const player = players[i];
-        ctx.fillRect(player.position.x, player.position.y, 10, 10);
-    }
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.fillStyle = "#151515";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	for (i in players) {
+		const player = players[i];
+		ctx.fillStyle = "red";
+		ctx.fillRect(player.position.x * 10, player.position.y * 10, 80, 80);
+		ctx.fillStyle = "blue";
+		ctx.textAlign = "center";
+		ctx.textBaseline = "middle";
+		ctx.font = "60px arial";
+		ctx.fillText(i, player.position.x * 10 + 40, player.position.y * 10 + 40);
+	}
 }
