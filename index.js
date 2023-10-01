27b252fdc05b77fa43e13af769a75046585c5ad6
Sultonov557 <sultonovzerifboy@gmail.com>
2023-10-01 21:32:36 +0500

    .

--- index.js
+++ index.js
@@ -21,7 +21,29 @@ server.on("connection", (socket) => {
 	let startPacket = { name: "start", params: { id } };
 	console.log("connected " + id);
 	socket.send(JSON.stringify(startPacket));
-	players[id] = new Player(id, new Position(50, 50));
+	players[id] = new Player(id, new Position(Math.floor(Math.random() * 180), Math.floor(Math.random() * 80)));
+	let bombs = 0;
+
+	for (i in players) {
+		if (players[i].bomb) {
+			if (bombs == 0) {
+				bombs++;
+			} else {
+				players[i].bomb = false;
+			}
+		}
+	}
+	if (bombs == 0) {
+		players[id].bomb = true;
+	}
+
+	for (i in players) {
+		let packet = {
+			name: "bomb",
+			params: { bomb: players[i].bomb, id: players[i].id },
+		};
+		broadcast(packet);
+	}
 	for (i in players) {
 		let packet = {
 			name: "position",
@@ -32,11 +54,16 @@ server.on("connection", (socket) => {
 	socket.on("message", (data) => {
 		data = JSON.parse(data);
 		if (data.name == "position") {
+			players[data.params.id].position = data.params.position;
 			let packet = {
 				name: "position",
 				params: { position: data.params.position, id: data.params.id },
 			};
 			broadcast(packet);
+		}
+		if (data.name == "bomb") {
+			players[data.id].bomb = data.bomb;
+			broadcast({ name: "bomb", params: { bomb: data.params.bomb, id: data.params.id } });
 		}
 	});
 	socket.on("close", () => {
@@ -45,17 +72,73 @@ server.on("connection", (socket) => {
 		console.log("disconnected " + id);
 	});
 
+	setInterval(() => {
+		for (i in players) {
+			let packet = {
+				name: "bomb",
+				params: { bomb: players[i].bomb, id: i },
+			};
+			broadcast(packet);
+		}
+	}, 100);
+
 	function broadcast(message) {
 		server.clients.forEach((client) => {
 			client.send(JSON.stringify(message));
 		});
 	}
 });
+let lastBomb;
+let lastBombTimer;
+let bombTimer = 1200;
+function tick() {
+	let bomber;
+	for (i in players) {
+		if (players[i].bomb) {
+			bomber = players[i];
+			bomber.id = i;
+			if (bombTimer == 0) {
+				console.log("game over");
+				server.clients.forEach((socket) => {
+					socket.send(JSON.stringify({ name: "gameover", params: { loser: bomber.id } }));
+					socket.close();
+				});
+				process.exit(1);
+			}
+		}
+	}
+	for (i in players) {
+		if (i != lastBomb) {
+			const playerPos = { x: players[i].position.x * 10 + 40, y: players[i].position.y * 10 + 40 };
+			const bombPos = { x: bomber.position.x * 10 + 40, y: bomber.position.y * 10 + 40 };
+			const dis = Math.floor(Math.sqrt(Math.sqrt((playerPos.x - bombPos.x) * (playerPos.x - bombPos.x) + (playerPos.y - bombPos.y) * (playerPos.y - bombPos.y))));
+			if (dis == 0) continue;
+			if (dis < 10) {
+				lastBomb = bomber.id;
+				lastBombTimer = 30;
+				bombTimer = 1200;
+				players[bomber.id].bomb = false;
+				players[i].bomb = true;
+			}
+		} else {
+			lastBombTimer--;
+			if (lastBombTimer == 0) {
+				lastBomb = undefined;
+			}
+		}
+	}
+	bombTimer--;
+	server.clients.forEach((socket) => {
+		socket.send(JSON.stringify({ name: "timer", params: { timer: bombTimer } }));
+	});
+}
+setInterval(tick, 100);
 
 class Player {
 	constructor(id, position) {
 		this.id = id;
 		this.position = position;
+		this.bomb = false;
 	}
 }
 
--- public/script.js
+++ public/script.js
@@ -6,6 +6,11 @@ const ctx = canvas.getContext("2d");
 let id;
 const players = {};
 
+let gameover = false;
+let loser;
+let timer = 0;
+const timerElm = document.querySelector("span");
+
 setInterval(frame, 10);
 
 client.addEventListener("message", (data) => {
@@ -17,9 +22,26 @@ client.addEventListener("message", (data) => {
 		if (!players[data.params.id]) players[data.params.id] = {};
 		players[data.params.id].position = data.params.position;
 	}
+	if (data.name == "bomb") {
+		if (!players[data.params.id]) players[data.params.id] = {};
+		players[data.params.id].bomb = data.params.bomb;
+	}
 	if (data.name == "remove") {
 		delete players[data.params.id];
 		console.log("removed " + data.params.id);
+	}
+	if (data.name == "timer") {
+		timer = data.params.timer;
+	}
+	if (data.name == "gameover") {
+		gameover = true;
+		loser = data.params.loser;
+		if (id == loser) {
+			alert("you lose");
+		} else {
+			alert("you won");
+		}
+		document.querySelector("canvas").style.display = "none";
 	}
 });
 
@@ -39,23 +61,23 @@ document.addEventListener("keypress", (ev) => {
 });
 
 setInterval(() => {
-	client.send(
-		JSON.stringify({ name: "position", params: { id, position: players[id].position } })
-	);
+	if (!gameover) {
+		client.send(JSON.stringify({ name: "position", params: { id, position: players[id].position } }));
+	}
 }, 10);
 
 function frame() {
+	timerElm.innerText = parseInt(timer / 10);
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.fillStyle = "#151515";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 	for (i in players) {
 		const player = players[i];
-		ctx.fillStyle = "red";
+		if (player.bomb) {
+			ctx.fillStyle = "red";
+		} else {
+			ctx.fillStyle = "blue";
+		}
 		ctx.fillRect(player.position.x * 10, player.position.y * 10, 80, 80);
-		ctx.fillStyle = "blue";
-		ctx.textAlign = "center";
-		ctx.textBaseline = "middle";
-		ctx.font = "60px arial";
-		ctx.fillText(i, player.position.x * 10 + 40, player.position.y * 10 + 40);
 	}
 }
